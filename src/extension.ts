aaa9d073358d023d28eba79836b84ee4d3f0c1b7
道化師 <wraith13@hotmail.co.jp>
2019-03-10 21:13:59 +0900

    🛠 実装

--- src/extension.ts
+++ src/extension.ts
@@ -24,20 +24,14 @@ export module BackgroundPhiColors
     const baseColor =
     {
         name: "baseColor",
-        defaultValue: "#FF666644",
-        value: "#FF666644",
+        defaultValue: "#CC666666",
+        value: "#CC666666",
     };
-    const hueCount =
-    {
-        name: "humCount",
-        minValue: 3,
-        maxValue: 47,
-        defaultValue: 13,
-        value: 13,
-    };
-    let decorations : { decorator: vscode.TextEditorDecorationType, rangesOrOptions: vscode.Range[] }[] = [];
+    let baseColorHsla: phiColors.Hsla;
+    let errorDecoration: { decorator: vscode.TextEditorDecorationType, rangesOrOptions: vscode.Range[] };
+    let decorations: { decorator: vscode.TextEditorDecorationType, rangesOrOptions: vscode.Range[] }[] = [];
 
-    export const getConfiguration = <type = vscode.WorkspaceConfiguration>(key? : string | Property<type>, section : string = applicationKey) : type =>
+    export const getConfiguration = <type = vscode.WorkspaceConfiguration>(key?: string | Property<type>, section: string = applicationKey): type =>
     {
         if (!key || "string" === typeof key)
         {
@@ -45,7 +39,7 @@ export module BackgroundPhiColors
             const rawSection = undefined === key || rawKey === key ? section: `${section}.${key.replace(/(.*)\.[^\.]+/, "$1")}`;
             const configuration = vscode.workspace.getConfiguration(rawSection);
             return rawKey ?
-            configuration[rawKey] :
+            configuration[rawKey]:
             configuration;
         }
         else
@@ -72,9 +66,10 @@ export module BackgroundPhiColors
             return result;
         }
     };
+
     export const getTicks = () => new Date().getTime();
 
-    export const initialize = (context : vscode.ExtensionContext): void =>
+    export const initialize = (context: vscode.ExtensionContext): void =>
     {
         context.subscriptions.push
         (
@@ -82,9 +77,9 @@ export module BackgroundPhiColors
             vscode.commands.registerCommand(`${applicationKey}.helloWorld`, async () => await vscode.window.showInformationMessage('Hello World!')),
 
             //  イベントリスナーの登録
-            vscode.window.onDidChangeActiveTextEditor(() => updateDecoration()),
-            vscode.workspace.onDidChangeTextDocument(() => updateDecoration()),
             vscode.workspace.onDidChangeConfiguration(() => onDidChangeConfiguration()),
+            vscode.window.onDidChangeActiveTextEditor(() => onDidChangeActiveTextEditor()),
+            vscode.workspace.onDidChangeTextDocument(() => onDidChangeTextDocument()),
         );
 
         onDidChangeConfiguration();
@@ -94,156 +89,307 @@ export module BackgroundPhiColors
     {
         getConfiguration(delay);
         getConfiguration(baseColor);
-        getConfiguration(hueCount);
-        const baseColorHsla = phiColors.rgbaToHsla(phiColors.rgbaFromStyle(baseColor.value));
+        baseColorHsla = phiColors.rgbaToHsla(phiColors.rgbaFromStyle(baseColor.value));
         decorations.forEach(i => i.decorator.dispose());
-        decorations = [];
-        decorations.push
-        (
-            {
-                decorator: createTextEditorDecorationType(baseColorHsla, 0, 1),
-                rangesOrOptions: []
-            }
-        );
-        for(let i = 1; i <= hueCount.value; ++i)
+        if (errorDecoration)
         {
-            decorations.push
-            (
-                {
-                    decorator: createTextEditorDecorationType(baseColorHsla, i),
-                    rangesOrOptions: []
-                }
-            );
+            errorDecoration.decorator.dispose();
         }
-        updateDecoration();
+        decorations = [];
+        errorDecoration =
+        {
+            decorator: createTextEditorDecorationType(baseColorHsla, 0),
+            rangesOrOptions: []
+        };
+
+        vscode.window.visibleTextEditors.forEach(i => updateDecoration(i));
     };
 
-    export const updateDecoration = (): void =>
-    {
-        const updateStamp = getTicks();
-        lastUpdateStamp = updateStamp;
-        setTimeout
-        (
-            () =>
-            {
-                if (lastUpdateStamp === updateStamp)
-                {
-                    delayedUpdateDecoration();
-                }
-            },
-            delay.value
-        );
-    };
-    export const getIndentSize = (text: string, tabSize: number) : number =>
-        text.replace(/\t/g, (s, offset) => s.repeat(tabSize -(offset %tabSize))).length;
-
-    const delayedUpdateDecoration = () =>
+    export const onDidChangeActiveTextEditor = (): void =>
     {
         const activeTextEditor = vscode.window.activeTextEditor;
         if (activeTextEditor)
         {
-            const text = activeTextEditor.document.getText();
-            const tabSize = undefined === activeTextEditor.options.tabSize ?
-                4:
-                (
-                    "number" === typeof activeTextEditor.options.tabSize ?
-                        activeTextEditor.options.tabSize:
-                        parseInt(activeTextEditor.options.tabSize)
-                );
-
-            //  clear
-            decorations.forEach(i => i.rangesOrOptions = []);
-
-            //  indent
-            const indents : { index: number, text: string }[] = [];
-            const indentRegexp = /^([ \t]+)([^\r\n]*)$/gm;
-            let totalSpaces = 0;
-            let totalTabs = 0;
-            const indentSizeDistribution :{ [key: string] : number } = { };
-            while(true)
-            {
-                const match = indentRegexp.exec(text);
-                if (null === match)
-                {
-                    break;
-                }
-                indents.push
-                (
-                    {
-                        index: match.index,
-                        text: match[1]
-                    }
-                );
-                const length = match[1].length;
-                const tabs = match[1].replace(/ /g, "").length;
-                const spaces = length -tabs;
-                totalSpaces += spaces;
-                totalTabs += tabs;
-                ++indentSizeDistribution[`size-${getIndentSize(match[1], tabSize)}`];
-            }
-            const isDefaultIndentCharactorSpace = totalTabs *tabSize <= totalSpaces;
-            //const indentUnit = isDefaultIndentCharactorSpace ? ddddd: tabSize;
-
-            indents.forEach
+            updateDecoration(activeTextEditor);
+        }
+    };
+    export const onDidChangeTextDocument = (): void =>
+    {
+        const activeTextEditor = vscode.window.activeTextEditor;
+        if (activeTextEditor)
+        {
+            const updateStamp = getTicks();
+            lastUpdateStamp = updateStamp;
+            setTimeout
             (
-                indent =>
+                () =>
                 {
-                    let hueIndex = 0;
-                    for(let i = 0; i < indent.text.length; ++i)
+                    if (lastUpdateStamp === updateStamp)
                     {
-                        if (hueCount.value <= hueIndex)
-                        {
-                            hueIndex = 0;
-                        }
-                        decorations[hueIndex +1].rangesOrOptions.push
-                        (
-                            new vscode.Range
-                            (
-                                activeTextEditor.document.positionAt(indent.index +i),
-                                activeTextEditor.document.positionAt(indent.index +i +1)
-                            )
-                        );
-                        ++hueIndex;
+                        updateDecoration(activeTextEditor);
                     }
-                }
-            );
-
-            //  trail
-            const trailRegexp = /^([^\r\n]*[^ \t\r\n]+)([ \t]+)$/gm;
-            while(true)
-            {
-                const match = trailRegexp.exec(text);
-                if (null === match)
-                {
-                    break;
-                }
-                console.log(`match: ${JSON.stringify(match)}`);
-                decorations[0].rangesOrOptions.push
-                (
-                    new vscode.Range
-                    (
-                        activeTextEditor.document.positionAt(match.index +match[1].length),
-                        activeTextEditor.document.positionAt(match.index +match[1].length +match[2].length)
-                    )
-                );
-            }
-
-            //  apply
-            decorations.forEach
-            (
-                i => activeTextEditor.setDecorations(i.decorator, i.rangesOrOptions)
+                },
+                delay.value
             );
         }
     };
-    export const createTextEditorDecorationType = (base: phiColors.Hsla, hue: number, alpha : number = 0.0): vscode.TextEditorDecorationType =>
-        vscode.window.createTextEditorDecorationType({backgroundColor: phiColors.rgbaForStyle(phiColors.hslaToRgba(phiColors.generate(base, hue, 0, 0, alpha)))});
+
+    export const gcd = (a: number, b: number) : number => b ? gcd(b, a % b): a;
+
+    export const getIndentSize = (text: string, tabSize: number): number =>
+        text.replace(/\t/g, (s, offset) => s.repeat(tabSize -(offset %tabSize))).length;
+
+    export const getIndentUnit =
+    (
+        indentSizeDistribution:{ [key: number]: number },
+        tabSize: number,
+        isDefaultIndentCharactorSpace: boolean
+    ): string =>
+    {
+        if (isDefaultIndentCharactorSpace)
+        {
+            let indentUnitSize = tabSize;
+            if (0 < Object.keys(indentSizeDistribution).length)
+            {
+                const sortedIndentSizeDistribution = Object.keys(indentSizeDistribution)
+                    .map(key => parseInt(key))
+                    .map(key => ({ indentSize: key, Count: indentSizeDistribution[key], }))
+                    .sort((a, b) => b.Count -a.Count);
+                if (1 < sortedIndentSizeDistribution.length)
+                {
+                    const failingLine = sortedIndentSizeDistribution[0].Count / 10;
+                    const topIndentSizeDistribution = sortedIndentSizeDistribution.filter((i, index) => failingLine < i.Count && index < 10);
+                    indentUnitSize = topIndentSizeDistribution.map(i => i.indentSize).reduce((a, b) => gcd(a, b));
+                }
+                else
+                {
+                    indentUnitSize = sortedIndentSizeDistribution[0].indentSize;
+                }
+            }
+            return " ".repeat(indentUnitSize);
+        }
+        return "\t";
+    };
+    
+    const makeRange = (textEditor: vscode.TextEditor, startPosition: number, endPosition: number) => new vscode.Range
+    (
+        textEditor.document.positionAt(startPosition),
+        textEditor.document.positionAt(endPosition)
+    );
+
+    const updateDecoration = (textEditor: vscode.TextEditor) =>
+    {
+        const text = textEditor.document.getText();
+        const tabSize = undefined === textEditor.options.tabSize ?
+            4:
+            (
+                "number" === typeof textEditor.options.tabSize ?
+                    textEditor.options.tabSize:
+                    parseInt(textEditor.options.tabSize)
+            );
+
+        //  clear
+        decorations.forEach(i => i.rangesOrOptions = []);
+        errorDecoration.rangesOrOptions = [];
+
+        //  indent
+        const indents: { index: number, text: string, body: string }[] = [];
+        const indentRegexp = /^([ \t]+)([^\r\n]*)$/gm;
+        let totalSpaces = 0;
+        let totalTabs = 0;
+        const indentSizeDistribution:{ [key: number]: number } = { };
+        while(true)
+        {
+            const match = indentRegexp.exec(text);
+            if (null === match)
+            {
+                break;
+            }
+            indents.push
+            (
+                {
+                    index: match.index,
+                    text: match[1],
+                    body: match[2]
+                }
+            );
+            const length = match[1].length;
+            const tabs = match[1].replace(/ /g, "").length;
+            const spaces = length -tabs;
+            totalSpaces += spaces;
+            totalTabs += tabs;
+            const indentSize = getIndentSize(match[1], tabSize);
+            if (indentSizeDistribution[indentSize])
+            {
+                ++indentSizeDistribution[indentSize];
+            }
+            else
+            {
+                indentSizeDistribution[indentSize] = 1;
+            }
+        }
+        const isDefaultIndentCharactorSpace = totalTabs *tabSize <= totalSpaces;
+        const indentUnit = getIndentUnit(indentSizeDistribution, tabSize, isDefaultIndentCharactorSpace);
+        const indentUnitSize = getIndentSize(indentUnit, tabSize);
+        //console.log(`indentSizeDistribution: ${JSON.stringify(indentSizeDistribution)}`);
+        //console.log(`indentUnit: ${JSON.stringify(indentUnit)}`);
+
+        indents.forEach
+        (
+            indent =>
+            {
+                let text = indent.text;
+                let cursor;
+                let nextCursor = indent.index;
+                for(let i = 0; 0 < text.length; ++i)
+                {
+                    cursor = nextCursor;
+                    if (text.startsWith(indentUnit))
+                    {
+                        if (decorations.length <= i)
+                        {
+                            decorations.push
+                            (
+                                {
+                                    decorator: createTextEditorDecorationType(baseColorHsla, i +1, -2),
+                                    rangesOrOptions: []
+                                }
+                            );
+                        }
+                        decorations[i].rangesOrOptions.push
+                        (
+                            makeRange
+                            (
+                                textEditor,
+                                cursor,
+                                nextCursor = cursor +indentUnit.length
+                            )
+                        );
+                        text = text.substr(indentUnit.length);
+                    }
+                    else
+                    {
+                        if (getIndentSize(text, tabSize) < indentUnitSize)
+                        {
+                            errorDecoration.rangesOrOptions.push
+                            (
+                                makeRange
+                                (
+                                    textEditor,
+                                    cursor,
+                                    nextCursor = cursor +text.length
+                                )
+                            );
+                            text = "";
+                        }
+                        else
+                        {
+                            if (isDefaultIndentCharactorSpace)
+                            {
+                                const spaces = text.length -text.replace(/^ +/, "").length;
+                                if (0 < spaces)
+                                {
+                                    decorations[i].rangesOrOptions.push
+                                    (
+                                        makeRange
+                                        (
+                                            textEditor,
+                                            cursor,
+                                            nextCursor = cursor +spaces
+                                        )
+                                    );
+                                    cursor = nextCursor;
+                                }
+                                errorDecoration.rangesOrOptions.push
+                                (
+                                    makeRange
+                                    (
+                                        textEditor,
+                                        cursor,
+                                        nextCursor = cursor +1
+                                    )
+                                );
+                                const indentCount = Math.ceil(getIndentSize(text.substr(0, spaces +1), tabSize) /indentUnitSize) -1;
+                                i += indentCount;
+                                text = text.substr(spaces +1);
+                            }
+                            else
+                            {
+                                const spaces = text.length -text.replace(/$ +/, "").length;
+                                errorDecoration.rangesOrOptions.push
+                                (
+                                    makeRange
+                                    (
+                                        textEditor,
+                                        cursor,
+                                        nextCursor = cursor +spaces
+                                        )
+                                );
+                                const indentCount = Math.ceil(spaces /indentUnitSize) -1;
+                                i += indentCount;
+                                text = text.substr(spaces);
+                            }
+                        }
+                    }
+                }
+            }
+        );
+
+        //  trail
+        const trailRegexp = /^([^\r\n]*[^ \t\r\n]+)([ \t]+)$/gm;
+        while(true)
+        {
+            const match = trailRegexp.exec(text);
+            if (null === match)
+            {
+                break;
+            }
+            errorDecoration.rangesOrOptions.push
+            (
+                makeRange
+                (
+                    textEditor,
+                    match.index +match[1].length,
+                    match.index +match[1].length +match[2].length
+                )
+            );
+        }
+
+        //  apply
+        decorations.forEach
+        (
+            i => textEditor.setDecorations(i.decorator, i.rangesOrOptions)
+        );
+        textEditor.setDecorations(errorDecoration.decorator, errorDecoration.rangesOrOptions);
+    };
+
+    export const createTextEditorDecorationType = (base: phiColors.Hsla, hue: number, alpha: number = 0.0): vscode.TextEditorDecorationType =>
+        vscode.window.createTextEditorDecorationType
+        (
+            {
+                backgroundColor: phiColors.rgbaForStyle
+                (
+                        phiColors.hslaToRgba
+                        (
+                            phiColors.generate
+                            (
+                                base,
+                                hue,
+                                0,
+                                0,
+                                alpha
+                            )
+                        )
+                ),
+            }
+        );
 }
 
-export function activate(context: vscode.ExtensionContext) : void
+export function activate(context: vscode.ExtensionContext): void
 {
     BackgroundPhiColors.initialize(context);
 }
 
-export function deactivate() : void
+export function deactivate(): void
 {
 }
