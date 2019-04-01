901553acacee6c344eec482f8fd27d60f87a2e2b
道化師 <wraith13@hotmail.co.jp>
2019-04-02 01:33:44 +0900

    🛠 設定値に誤りがあってもメッセージはログに出力するだけに留める

--- src/extension.ts
+++ src/extension.ts
@@ -132,7 +132,9 @@ export module BackgroundPhiColors
             let result = value;
             if (this.validator && !this.validator(result))
             {
-                vscode.window.showErrorMessage(`${rawKey} setting value is invalid! Please check your settings.`);
+                // settings.json をテキストとして直接編集してる時はともかく GUI での編集時に無駄にエラー表示が行われてしまうので、エンドユーザーに対するエラー表示は行わない。
+                //vscode.window.showErrorMessage(`${rawKey} setting value is invalid! Please check your settings.`);
+                console.error(`${rawKey} setting value is invalid! Please check your settings.`);
                 result = this.defaultValue;
             }
             else
